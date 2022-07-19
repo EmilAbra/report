5eb51df72da477fa74c80b5ebf42c4d50f01197f
eabra <emab21@student.bth.se>
2022-07-19 02:03:58 +0200

    add library, show all books, edit book

---
+++ src/Controller/BooksController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Books;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\BooksRepository;
+
+class BooksController extends AbstractController
+{
+    #[Route('/books', name: 'app_books')]
+    public function index(): Response
+    {
+        return $this->render('books/index.html.twig');
+    }
+
+    /**
+    * @Route("/books/show", name="books_show_all")
+    */
+    public function showAllBooks(
+        BooksRepository $booksRepository
+    ): Response {
+        $books = $booksRepository
+            ->findAll();
+
+        if (!$books) {
+                throw $this->createNotFoundException(
+                    'No books found in database'
+                );
+            }
+
+        return $this->render('books/show_all.html.twig', [
+            'books' => $books
+        ]);
+    }
+
+    /**
+    * @Route("/books/show/{id}", name="books_show_one")
+    */
+    public function showOneBook(
+        BooksRepository $booksRepository, int $id
+    ): Response {
+        $book = $booksRepository
+            ->find($id);
+
+        if (!$book) {
+                throw $this->createNotFoundException(
+                    'No book found for id '.$id
+                );
+            }
+
+        return $this->render('books/show_one.html.twig', [
+            'book' => $book
+        ]);
+    }
+
+    /**
+    * @Route("/books/update", name="books_update")
+    */
+    public function updateBook(
+        BooksRepository $booksRepository
+    ): Response {
+        $books = $booksRepository
+            ->findAll();
+
+        if (!$books) {
+                throw $this->createNotFoundException(
+                    'No books found in database'
+                );
+            }
+
+        return $this->render('books/update_book.html.twig', [
+            'books' => $books
+        ]);
+    }
+
+    /**
+    * @Route("/books/update/{id}", name="books_update_form", methods={"GET"})
+    */
+    public function updateOneBook(
+        BooksRepository $booksRepository, int $id
+    ): Response {
+        $book = $booksRepository
+            ->find($id);
+
+        if (!$book) {
+                throw $this->createNotFoundException(
+                    'No book found for id '.$id
+                );
+            }
+
+        return $this->render('books/book_edit_form.html.twig', [
+            'book' => $book
+        ]);
+    }
+
+    /**
+     * @Route("/books/update/{id}", name="books_update_process", methods={"GET", "POST"})
+     */
+    public function updateProcess(ManagerRegistry $doctrine, Request $request, int $id): Response
+    {
+        $author = $request->request->get('author');
+        $title  = $request->request->get('title');
+        $isbn  = $request->request->get('isbn');
+        $imgpath  = $request->request->get('imgpath');
+
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Books::class)->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+
+        $book->setAuthor($author);
+        $book->setTitel($title);
+        $book->setIsbn($isbn);
+        $book->setImgpath($imgpath);
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('books_update');
+    }
+
+    /**
+     * @Route("/books/create", name="books_create_form", methods={"GET","HEAD"})
+     */
+    // public function createBook(): Response
+    // {
+    //     return $this->render('books/book_create_form.html.twig');
+    // }
+
+    /**
+     * @Route("/books/create", name="books_create_process", methods={"POST"})
+     */
+    // public function createBookProcess(
+    //     ManagerRegistry $doctrine, Request $request
+    // ): Response
+    // {
+    //     $author = $request->request->get('author');
+    //     $title  = $request->request->get('title');
+    //     $isbn  = $request->request->get('isbn');
+    //     $imgpath  = $request->request->get('imgpath');
+    //
+    //     $entityManager = $doctrine->getManager();
+    //
+    //     $book = new Books();
+    //     $book->setAuthor($author);
+    //     $book->setTitel($title);
+    //     $book->setIsbn($isbn);
+    //     $book->setImgpath($imgpath);
+    //
+    //     $entityManager->persist($book);
+    //
+    //     $entityManager->flush();
+    //
+    //     return $this->redirectToRoute('books/show_all.html.twig');
+    // }
+}
---
+++ src/Entity/Books.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BooksRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BooksRepository::class)]
+class Books
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $titel = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $isbn = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $author = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imgpath = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitel(): ?string
+    {
+        return $this->titel;
+    }
+
+    public function setTitel(string $titel): self
+    {
+        $this->titel = $titel;
+
+        return $this;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getImgpath(): ?string
+    {
+        return $this->imgpath;
+    }
+
+    public function setImgpath(string $imgpath): self
+    {
+        $this->imgpath = $imgpath;
+
+        return $this;
+    }
+}
