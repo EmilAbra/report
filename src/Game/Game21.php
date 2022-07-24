b975d89534ea5dabd3f6828c74293fc0c530f496
eabra <emab21@student.bth.se>
2022-07-25 01:56:48 +0200

    remove saldo methods

--- src/Game/Game21.php
+++ src/Game/Game21.php
@@ -51,43 +51,25 @@ class Game21
      * else null.
      *
      * Method to handle players score with a return message if score is 21 or
-     * over 21, and updates the * money saldos by calling method updateSaldo.
+     * over 21, and updates the money saldos by calling method setSaldo.
      * If score is over 21: calls method fixIfAcesInHand to lower score of Ace to 1.
      * Else returns null so the player can choose again.
      */
     public function handlePlayerScore(int $score, int $betAmount): mixed
     {
         if ($score === 21) {
-            $this->updateSaldo($betAmount, -$betAmount);
+            $this->player->setSaldo($betAmount);
+            $this->dealer->setSaldo(-$betAmount);
             return "Du fick 21!!! Grattis du vann denna omgången!";
         } elseif ($score > 21) {
             if ($this->cardValues->fixIfAcesInHand($this->player) > 21) {
-                $this->updateSaldo(-$betAmount, $betAmount);
+                $this->player->setSaldo(-$betAmount);
+                $this->dealer->setSaldo($betAmount);
                 return "Ajdå, Du fick över 21!";
             }
         }
         return null;
     }
-
-    // /**
-    //  * @param Deck $deck - Deck object.
-    //  * @param int $betAmount - Money the player has placed in the bet.
-    //  * @return mixed - returns string with message if score is 21 or over 21,
-    //  * else null, by calling method handlePlayerScore.
-    //  *
-    //  * Method to deal player a card and update players cardpoints. Call methods
-    //  * handlePlayerScore to check if score is over 21 or equal to 21.
-    //  */
-    // public function dealPlayer(Deck $deck, int $betAmount): mixed
-    // {
-    //     $card = $this->dealer->deal($deck);
-    //     $this->player->setCardHand($card);
-    //     $points = $this->cardValues->getValue($card);
-    //     $this->player->setScore($points);
-    //     $score = $this->player->getScore();
-    //
-    //     return $this->handlePlayerScore($score, $betAmount);
-    // }
 
     /**
      * @param int $betAmount - Money the player has placed in the bet.
@@ -115,48 +97,21 @@ class Game21
      * the player has better score than the bank. Else null.
      *
      * Method to handle banks score with a return message, and updates the
-     * money saldos by calling method updateSaldo.
+     * money saldos by calling method setSaldo.
      */
     public function handleBankScore(int $score, int $betAmount): string
     {
         if ($score === 21) {
-            $this->updateSaldo(-$betAmount, $betAmount);
+            $this->player->setSaldo(-$betAmount);
+            $this->dealer->setSaldo($betAmount);
             return "Aj, Banken fick 21!";
         } elseif ($score >= $this->player->getScore()) {
-            $this->updateSaldo(-$betAmount, $betAmount);
+            $this->player->setSaldo(-$betAmount);
+            $this->dealer->setSaldo($betAmount);
             return "Ajdå, Banken hade bättre kort!";
         }
         return "Bra, Du hade bättre kort än Banken!!!";
     }
-
-    // /**
-    //  * @param Deck $deck - Deck object.
-    //  * @param int $betAmount - Money the player has placed in the bet.
-    //  * @return mixed - returns string with message if score is 21 or over 21,
-    //  * else null, by calling method handlePlayerScore.
-    //  *
-    //  * While loop to deal the bank dealer cards until the points is over 16.
-    //  * If score is over 21: calls method fixIfAcesInHand to lower score of Ace to 1.
-    //  * If score still over 21: Calls method updateSaldo and returns a message.
-    //  * After loop calls handleBankScore to get and return a proper message based on the score.
-    //  */
-    // public function dealBank(Deck $deck, int $betAmount): mixed
-    // {
-    //     while ($this->dealer->getScore() < 17) {
-    //         $card = $this->dealer->deal($deck);
-    //         $this->dealer->setCardHand($card);
-    //         $points = $this->cardValues->getValue($card);
-    //         $this->dealer->setScore($points);
-    //         if ($this->dealer->getScore() > 21) {
-    //             if ($this->cardValues->fixIfAcesInHand($this->dealer) > 21) {
-    //                 $this->updateSaldo($betAmount, -$betAmount);
-    //                 return "Grattis, Banken fick över 21!!!";
-    //             }
-    //         }
-    //     }
-    //     $score = $this->dealer->getScore();
-    //     return $this->handleBankScore($score, $betAmount);
-    // }
 
     /**
      * @param int $betAmount - Money the player has placed in the bet.
@@ -165,7 +120,7 @@ class Game21
      *
      * While loop to deal the bank dealer cards until the points is over 16.
      * If score is over 21: calls method fixIfAcesInHand to lower score of Ace to 1.
-     * If score still over 21: Calls method updateSaldo and returns a message.
+     * If score still over 21: Calls method setSaldo and returns a message.
      * After loop calls handleBankScore to get and return a proper message based on the score.
      */
     public function bankTurn(int $betAmount): mixed
@@ -177,42 +132,13 @@ class Game21
             $this->dealer->setScore($points);
             if ($this->dealer->getScore() > 21) {
                 if ($this->cardValues->fixIfAcesInHand($this->dealer) > 21) {
-                    $this->updateSaldo($betAmount, -$betAmount);
+                    $this->player->setSaldo($betAmount);
+                    $this->dealer->setSaldo(-$betAmount);
                     return "Grattis, Banken fick över 21!!!";
                 }
             }
         }
         $score = $this->dealer->getScore();
         return $this->handleBankScore($score, $betAmount);
-    }
-
-    /**
-     * @return mixed - returns string with message if player or bank saldo is les than 1.
-     * Else null to let the game continue.
-     *
-     * Check both player and bank saldo.
-     */
-    public function checkPlayerSaldo(): mixed
-    {
-        if ($this->player->getMoney() < 1) {
-            return "GAME OVER. Dina pengar är slut.";
-        }
-        if ($this->dealer->getMoney() < 1) {
-            return "GRATTIS DU VANN!!! Bankens pengar är slut.";
-        }
-        return null;
-    }
-
-    /**
-     * @param $playeramount - bet amount to be changed for players saldo.
-     * @param $dealeramount - bet amount to be changed for dealers saldo.
-     * @return void
-     *
-     * Update both Player and Banks saldo.
-     */
-    public function updateSaldo(int $playerAmount, int $dealerAmount): void
-    {
-        $this->player->setMoney($playerAmount);
-        $this->dealer->setMoney($dealerAmount);
     }
 }
